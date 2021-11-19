1f5279fb7521440c8a964accf7b19a57cf59d739
Decryptus007 <domnick080@outlook.com>
2021-11-19 02:09:53 +0100

    gallery

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCDjVGGsAMZ9ifaOjNSl3S85caWhitSw7c",
+    authDomain: "dom-firegram.firebaseapp.com",
+    databaseURL: "https://dom-firegram.firebaseio.com",
+    projectId: "dom-firegram",
+    storageBucket: "dom-firegram.appspot.com",
+    messagingSenderId: "862364904085",
+    appId: "1:862364904085:web:f687efe9aaf6c36b37099c"
+}
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig)
+
+  const projectStorage = firebase.storage()
+  const projectFirestore = firebase.firestore()
+
+  export { projectStorage, projectFirestore }
